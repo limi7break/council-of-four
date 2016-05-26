56e90b9624fcde12614d31f46e2f8349715938dd
Michele Ferri <tiduspira@gmail.com>
2016-05-26 22:49:33 +0200

    ClientConnection interface: methods to send requests and get responses.

---
+++ src/main/java/it/polimi/ingsw/ps13/view/client/ClientConnection.java
@@ -0,0 +1,12 @@
+package it.polimi.ingsw.ps13.view.client;
+
+import it.polimi.ingsw.ps13.message.request.RequestMsg;
+import it.polimi.ingsw.ps13.message.response.ResponseMsg;
+
+public interface ClientConnection {
+
+	public ResponseMsg receiveMessage();
+	
+	public void sendMessage(RequestMsg msg);
+	
+}
