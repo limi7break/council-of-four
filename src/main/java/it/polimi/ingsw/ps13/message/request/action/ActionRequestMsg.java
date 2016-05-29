e29e63a9e521d0dc8bb759b0e48c96e96304d223
Michele Ferri <tiduspira@gmail.com>
2016-05-30 00:04:25 +0200

    Small fix to remove errors

--- src/main/java/it/polimi/ingsw/ps13/message/request/action/ActionRequestMsg.java
+++ src/main/java/it/polimi/ingsw/ps13/message/request/action/ActionRequestMsg.java
@@ -3,11 +3,26 @@ package it.polimi.ingsw.ps13.message.request.action;
 import it.polimi.ingsw.ps13.controller.actions.Action;
 import it.polimi.ingsw.ps13.controller.actions.ActionVisitor;
 import it.polimi.ingsw.ps13.message.request.RequestMsg;
+import it.polimi.ingsw.ps13.model.player.Player;
 
 public abstract class ActionRequestMsg extends RequestMsg {
 
 	private static final long serialVersionUID = 0L;
 	
+	private Player player;
+	
 	abstract Action accept(ActionVisitor av);
+	
+	public Player getPlayer() {
+		
+		return player;
+		
+	}
+	
+	public void setPlayer(Player player) {
+		
+		this.player = player;
+		
+	}
 	
 }
